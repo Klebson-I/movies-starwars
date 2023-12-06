c77617098b30396105a0f3c0fa7ad5e1f8d96f88
lkleba <lukasz.kleba.ext@nokia.com>
2023-12-06 12:57:51 +0100

    create draft of controller, service and module for species endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FilmModule } from './film/film.module';
+import { SpeciesModule } from './species/species.module';
 
 @Module({
-  imports: [FilmModule],
+  imports: [FilmModule, SpeciesModule],
   controllers: [AppController],
   providers: [AppService],
 })
