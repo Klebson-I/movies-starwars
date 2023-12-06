9dc04b49f6af8a5470c35ca03aea9ed8bc824847
lkleba <lukasz.kleba.ext@nokia.com>
2023-12-06 13:01:08 +0100

    create draft of controller, service and module for planet endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,13 @@ import { VehicleService } from './vehicle/vehicle.service';
 import { VehicleController } from './vehicle/vehicle.controller';
 import { VehicleModule } from './vehicle/vehicle.module';
 import { StarshipModule } from './starship/starship.module';
+import { PlanetService } from './planet/planet.service';
+import { PlanetController } from './planet/planet.controller';
+import { PlanetModule } from './planet/planet.module';
 
 @Module({
-  imports: [FilmModule, SpeciesModule, VehicleModule, StarshipModule],
-  controllers: [AppController, VehicleController],
-  providers: [AppService, VehicleService],
+  imports: [FilmModule, SpeciesModule, VehicleModule, StarshipModule, PlanetModule],
+  controllers: [AppController, VehicleController, PlanetController],
+  providers: [AppService, VehicleService, PlanetService],
 })
 export class AppModule {}
