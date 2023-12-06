39f2e493122af0224c382e5dd75d651ef66f23cd
lkleba <lukasz.kleba.ext@nokia.com>
2023-12-06 12:59:47 +0100

    create draft of controller, service and module for starship endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { SpeciesModule } from './species/species.module';
 import { VehicleService } from './vehicle/vehicle.service';
 import { VehicleController } from './vehicle/vehicle.controller';
 import { VehicleModule } from './vehicle/vehicle.module';
+import { StarshipModule } from './starship/starship.module';
 
 @Module({
-  imports: [FilmModule, SpeciesModule, VehicleModule],
+  imports: [FilmModule, SpeciesModule, VehicleModule, StarshipModule],
   controllers: [AppController, VehicleController],
   providers: [AppService, VehicleService],
 })
---
+++ src/starship/starship.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { StarshipController } from './starship.controller';
+import { StarshipService } from './starship.service';
+
+@Module({
+  controllers: [StarshipController],
+  providers: [StarshipService]
+})
+export class StarshipModule {}
