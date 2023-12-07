48e6f54380349c43f544ca610550558557e8274c
Łukasz Klęba <lukasz.kleba99@onet.pl>
2023-12-07 19:37:20 +0100

    create draft od controller and service for /species endpoint

--- src/species/species.service.ts
+++ src/species/species.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SpeciesService {}
+export class SpeciesService {
+  constructor() {}
+
+  async getAllSpecies() {}
+
+  async getSpecies(id: string) {}
+}
