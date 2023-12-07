48a7ab6a56dabbb9dab28f556ffb8993974763d1
Łukasz Klęba <lukasz.kleba99@onet.pl>
2023-12-07 21:14:34 +0100

    create starship entity and repository

--- src/starship/starship.service.ts
+++ src/starship/starship.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Starship } from 'src/Database/Starship/Starship.entity';
+import { StarshipRepository } from 'src/Database/Starship/StarshipEntity.repository';
 
 @Injectable()
 export class StarshipService {
-  constructor() {}
+  constructor(
+    @InjectRepository(Starship) private starshipRepository: StarshipRepository,
+  ) {}
 
   async getAllStarships() {}
 
