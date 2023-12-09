aa7068294744cb7049ce15875cc20ab3871d2d19
Łukasz Klęba <lukasz.kleba99@onet.pl>
2023-12-09 18:28:12 +0100

    create tests for getMostOftenPeople foo, create file with mocks

---
+++ src/__test/getMostOftenPeople/mocks.ts
@@ -0,0 +1,22 @@
+export const openingsMockSingleResult = [
+  'Luke Skywalker is taken by Darth Vader on the ship where they must fight',
+  'Luke Skywalker meets Jabba, Tadeusz and Dooku on a party',
+];
+
+export const openingsMockMultiResult = [
+  'Luke Skywalker is taken by Darth Vader on the ship where they must fight',
+  'Luke Skywalker meets Jabba, Tadeusz and Dooku on a party',
+  'Darth Vader is pretty cool',
+];
+
+export const peopleNamesMock = [
+  'Luke Skywalker',
+  'Darth Vader',
+  'Dooku',
+  'Jabba',
+  'Tadeusz',
+];
+
+export const expectedSingleMostOftenResult = 'Luke';
+
+export const expectedMultiMostOftenResult = ['Luke', 'Darth'];
